3c4d757822c01f85d534757ea071c5142bbffc4e
AryaKS01 <aryaworks24@gmail.com>
2024-09-12 21:37:13 +0530

    web dev

---
+++ Javascript Practice/SilkieDOM.js
@@ -0,0 +1,20 @@
+// const allImages = document.getElementsByTagName("img");
+
+// for(let img of allImages){
+//     // console.log(img.src);
+//     img.src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg";
+// }
+
+// const links = document.querySelectorAll("p a");
+
+// for(let link of links){
+//     console.log(link.href);
+// }
+
+const links = document.querySelectorAll("a");
+
+for(let link of links){
+    // link.innerText="yyoyoyoyoyoyoyoyoyoy!!!!";
+    link.style.color = "green";
+    
+}
