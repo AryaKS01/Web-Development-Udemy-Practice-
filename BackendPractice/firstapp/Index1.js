4031966c224c0079779207980249635369a1e985
AryaKS01 <aryaworks24@gmail.com>
2024-10-08 22:24:45 +0530

    Oct

--- BackendPractice/firstapp/Index1.js
+++ BackendPractice/firstapp/Index1.js
@@ -1,30 +1,33 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const methodOverride=require("method-override");
+const {v4:uuid}=require("uuid");
 
 app.use(express.urlencoded ({extended:true}))
 app.use(express.json())
+app.use(methodOverride("_method"))
 app.set("views",path.join(__dirname,"views1"));
 app.set("view engine","ejs");
 
 const comments=[
     {
-        id:1,
+        id:uuid(),
         username:"Todd",
         comment:"lol thats funny"
     },
     {
-        id:2,
+        id:uuid(),
         username:"Skyler",
         comment:"i like to bidwatch"
     },
     {
-        id:3,
+        id:uuid(),
         username:"Arya",
         comment:"delete this todd "
     },
     {
-        id:4,
+        id:uuid(),
         username:"onlysaywoof",
         comment:"woof woof woof"
     }
@@ -41,15 +44,31 @@ app.get("/comments/new",(req,res)=>{
 app.post("/comments",(req,res)=>{
     // console.log(req.body);
     const {username, comment}=req.body;
-    comments.push({username, comment})
+    comments.push({username, comment,id:uuid()})
     // res.send("It worked");
     res.redirect("/comments");
 })
 
 app.get("/comments/:id",(req,res)=>{
     const {id}=req.params;
-    const comment=comments.find(c=>c.id===parseInt(id));
+    const comment=comments.find(c=>c.id===id);
     res.render("comments/show",{comment});
+})
+
+app.get("/comments/:id/edit",(req,res)=>{
+    const {id}=req.params;
+    const comment=comments.find(c=>c.id===id);
+    res.render("comments/edit",{comment})
+})
+
+app.patch("/comments/:id", (req,res)=>{
+    const {id}=req.params;
+    // console.log(req.body.comment);
+    // res.send("updating something!")
+    const newCommentText=req.body.comment;
+    const foundComment=comments.find(c=>c.id===id);
+    foundComment.comment=newCommentText;
+    res.redirect("/comments");
 })
 
 app.get("/tacos", (req,res)=>{
