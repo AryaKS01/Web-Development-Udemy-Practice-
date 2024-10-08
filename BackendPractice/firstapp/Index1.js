1675c1dd5bc4bd2877706e811bd55c1b1ce6480a
AryaKS01 <aryaworks24@gmail.com>
2024-10-08 20:04:31 +0530

    OCt

--- BackendPractice/firstapp/Index1.js
+++ BackendPractice/firstapp/Index1.js
@@ -9,18 +9,22 @@ app.set("view engine","ejs");
 
 const comments=[
     {
+        id:1,
         username:"Todd",
         comment:"lol thats funny"
     },
     {
+        id:2,
         username:"Skyler",
         comment:"i like to bidwatch"
     },
     {
+        id:3,
         username:"Arya",
         comment:"delete this todd "
     },
     {
+        id:4,
         username:"onlysaywoof",
         comment:"woof woof woof"
     }
@@ -28,6 +32,24 @@ const comments=[
 
 app.get("/comments",(req,res)=>{
     res.render("comments/index1", {comments})
+})
+
+app.get("/comments/new",(req,res)=>{
+    res.render("comments/new1");
+})
+
+app.post("/comments",(req,res)=>{
+    // console.log(req.body);
+    const {username, comment}=req.body;
+    comments.push({username, comment})
+    // res.send("It worked");
+    res.redirect("/comments");
+})
+
+app.get("/comments/:id",(req,res)=>{
+    const {id}=req.params;
+    const comment=comments.find(c=>c.id===parseInt(id));
+    res.render("comments/show",{comment});
 })
 
 app.get("/tacos", (req,res)=>{
